0e7753ce3d3528f3f5587e4b748b2fe28f51419f
Eugene <freeedayy@gmail.com>
2023-03-12 13:00:48 +0300

    useThrottle hook implementation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import './App.css';
-import {useArray} from "./useArray";
-import {useDebounce} from "./useDebounce";
+import {useThrottle} from "./useThrottle";
+import {useState} from "react";
 
 function App() {
-    const [values, {push, update, filter, remove, clear}] = useArray([0, 0, 1,2,3,4,5,6]);
-
-    useDebounce(() => {
-        filter(a => a !== 0)
-    }, 1000, [values]);
-
-    // useUpdateEffect(() => {
-    //     alert(count);
-    // }, [count]);
+    const [value, setValue] = useState('');
+    const throttleValue = useThrottle(value);
 
     return (
         <div className="App">
             <header className="App-header">
                 <p>
-                    [{values.join(',')}]
+                    {throttleValue}
                 </p>
 
-                <button onClick={() => push(values.length)}>Push</button>
-
-                <button onClick={() => update(3, 777)}>Update</button>
-
-                <button onClick={() => remove(3)}>Remove</button>
-
-                <button onClick={() => clear()}>Clear</button>
+                <input value={value} onChange={(e) => setValue(e.target.value)} />
 
             </header>
         </div>
